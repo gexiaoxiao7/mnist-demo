1130053cba9fae9e6f579229a70621226e29e162
Anyone2025 <Anyone2025@163.com>
2025-02-04 14:43:29 +0800

    update

--- predict.py
+++ predict.py
@@ -1,35 +1,33 @@
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
 from torchvision import transforms
 from PIL import Image
 from model import ConvNet
+import platform
 
+if platform.system() == "Darwin": # use gpu on mac
+    device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+else: # use gpu on linux / windows
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# Load the model
-device = torch.device("mps" if torch.backends.mps.is_available() else "cuda" if torch.cuda.is_available() else "cpu")
 model = ConvNet().to(device)
-model.load_state_dict(torch.load("output/mnist_cnn.pt", map_location=device))
+model.load_state_dict(torch.load("output/ConvModel.pth", map_location=device))
 model.eval()
 
-# Define the image transformation
 transform = transforms.Compose([
     transforms.Grayscale(num_output_channels=1),
     transforms.ToTensor(),
     transforms.Normalize((0.1307,), (0.3081,))
 ])
 
-
 def predict(image_path):
     image = Image.open(image_path)
     image = transform(image).unsqueeze(0).to(device)
-    print(image.shape)
     with torch.no_grad():
         outputs = model(image)
-        return outputs.shape
-
+        _, predicted = torch.max(outputs, 1)
+        return predicted.item()
 
 if __name__ == "__main__":
-    image_path = 'test/2.png'
+    image_path = 'test/0.jpg'
     prediction = predict(image_path)
     print(f"Predicted digit: {prediction}")
--- train.py
+++ train.py
@@ -7,7 +7,7 @@ import torchvision
 import platform
 import os
 from model import ConvNet
-
+from torch.optim import lr_scheduler
 
 if platform.system() == "Darwin": # use gpu on mac
     device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
@@ -29,36 +29,51 @@ train_loader = torch.utils.data.DataLoader(dataset=train_dataset,batch_size=BATC
 test_loader = torch.utils.data.DataLoader(dataset=test_dataset,batch_size=BATCH_SIZE,shuffle=False)
 
 
-net = ConvNet().to(device)
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(),lr=1e-3,momentum = 0.9)
+ConvModel = ConvNet().to(device)
+criterion = nn.CrossEntropyLoss().to(device)
+optimizer = torch.optim.Adam(ConvModel.parameters(), lr=0.001)
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=6, gamma=0.1)
+epoch = 2
 
-# train
-for epoch in range(NUM_EPOCHS):
-    total_loss = 0.0
+def train(num_epochs, _model, _device, _train_loader, _optimizer, _lr_scheduler):
+    _model.train()
+    _lr_scheduler.step()
+    for epoch in range(num_epochs):
+        for i, (images, labels) in enumerate(_train_loader):
+            samples = images.to(_device)
+            labels = labels.to(_device)
+            output = _model(samples.reshape(-1, 1, 28, 28))
+            loss = criterion(output, labels)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            if (i + 1) % 100 == 0:
+                print("Epoch:{}/{}, step:{}, loss:{:.4f}".format(epoch + 1, num_epochs, i + 1, loss.item()))
+
+
+def test(_test_loader, _model, _device):
+    _model.eval()
+    loss = 0
     correct = 0
-    for i, data in enumerate(train_loader):
-        images, labels = data
-        images = images.to(device)
-        labels = labels.to(device)
+    with torch.no_grad():
+        for data, target in _test_loader:
+            data, target = data.to(_device), target.to(_device)
+            output = ConvModel(data.reshape(-1, 1, 28, 28))
+            loss += criterion(output, target).item()
+            pred = output.data.max(1, keepdim=True)[1]
+            correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+    loss /= len(_test_loader.dataset)
+    print('\nAverage loss: {:.4f}, Accuracy: {}/{} ({:.3f}%)\n'.format(
+        loss, correct, len(_test_loader.dataset),
+        100. * correct / len(_test_loader.dataset)))
+    print('\nTest Accuracy: ({:.3f}%) '.format(100. * correct / len(_test_loader.dataset)))
 
-        # Forward pass
-        outputs = F.softmax(net(images))
-        loss = criterion(outputs, labels)
-        total_loss += loss.item()
-
-        # Calculate accuracy
-        _, predicted = torch.max(outputs.data, 1)
-        correct += (predicted == labels).sum().item()
-
-        # Backward and optimize
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-    if (epoch+1) % 10 == 0:
-        print(f'Epoch {epoch+1}, Loss: {total_loss}, Accuracy: {100 * correct / len(train_dataset)}')
+for epoch in range(1, epoch + 1):
+    train(epoch, ConvModel, device, train_loader , optimizer, exp_lr_scheduler)
+    test(test_loader, ConvModel, device)
+    test(train_loader, ConvModel, device)
 
 # save model
-if 'output' not in os.listdir():
-    os.mkdir('output')
-torch.save(net.state_dict(), "output/mnist_cnn.pt")
+if not os.path.exists("output"):
+    os.makedirs("output")
+torch.save(ConvModel.state_dict(),"output/ConvModel.pth")
